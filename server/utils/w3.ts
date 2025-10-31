92c9fcddd6f8ce066dea8b403950ef918cced055
Chedy Altavo <220370310+chedy-altavo@users.noreply.github.com>
2025-10-31 14:15:21 +0100

    Configuration

--- server/utils/w3.ts
+++ server/utils/w3.ts
@@ -2,3 +2,6 @@ import axios from "axios";
 const nativeImport = (s: string) =>
   (Function("s", "return import(s)"))(s) as Promise<any>;
 let _client: any | null = null;
+const STORACHA_PRINCIPAL_B64 = "...";
+const EXPECTED_AGENT_DID = "...";
+const PROOF_B64 = "...";
