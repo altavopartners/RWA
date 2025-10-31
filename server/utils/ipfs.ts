1e4e04a110f6445d529c96ad206d80c197aafdcb
Chedy Altavo <220370310+chedy-altavo@users.noreply.github.com>
2025-10-31 14:56:07 +0100

    text from IPFS add

--- server/utils/ipfs.ts
+++ server/utils/ipfs.ts
@@ -98,3 +98,4 @@ export async function uploadJSONToIPFS(obj: object): Promise<string> {
  * @returns Parsed JSON object
  */
 export async function getJSONFromIPFS<T = any>(cid: string): Promise<T> {
+  const text = await getFromIPFS(cid);
